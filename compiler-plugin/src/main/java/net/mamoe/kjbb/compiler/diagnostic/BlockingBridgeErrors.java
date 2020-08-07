dc12ef6e64cd4148a8df41b830376f3084f9ca78
Him188 <Him188@mamoe.net>
2020-08-07 17:44:15 +0800

    Rise INAPPLICABLE_JVM_BLOCKING_BRIDGE to ERROR

--- compiler-plugin/src/main/java/net/mamoe/kjbb/compiler/diagnostic/BlockingBridgeErrors.java
+++ compiler-plugin/src/main/java/net/mamoe/kjbb/compiler/diagnostic/BlockingBridgeErrors.java
@@ -5,12 +5,13 @@ import org.jetbrains.kotlin.diagnostics.DiagnosticFactory0;
 import org.jetbrains.kotlin.diagnostics.DiagnosticFactory1;
 import org.jetbrains.kotlin.diagnostics.Errors;
 
+import static org.jetbrains.kotlin.diagnostics.Severity.ERROR;
 import static org.jetbrains.kotlin.diagnostics.Severity.WARNING;
 
 
 public interface BlockingBridgeErrors {
     DiagnosticFactory0<PsiElement> PLUGIN_IS_NOT_ENABLED = DiagnosticFactory0.create(WARNING);
-    DiagnosticFactory0<PsiElement> INAPPLICABLE_JVM_BLOCKING_BRIDGE = DiagnosticFactory0.create(WARNING);
+    DiagnosticFactory0<PsiElement> INAPPLICABLE_JVM_BLOCKING_BRIDGE = DiagnosticFactory0.create(ERROR);
     DiagnosticFactory0<PsiElement> REDUNDANT_JVM_BLOCKING_BRIDGE_ON_PRIVATE_DECLARATIONS = DiagnosticFactory0.create(WARNING);
 
     DiagnosticFactory1<PsiElement, String> OVERRIDING_GENERATED_BLOCKING_BRIDGE = DiagnosticFactory1.create(WARNING);
