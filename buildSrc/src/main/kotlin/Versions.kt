f74acf42b80b81438c1de8b1d26b564720c738db
Him188 <Him188@mamoe.net>
2020-08-12 13:31:38 +0800

    0.9.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val project = "0.9.0"
-    const val idePlugin = "1.1.0"
+    const val idePlugin = "1.2.0"
 
     const val kotlin = "1.4.0-rc"
     const val coroutines = "1.3.8"
