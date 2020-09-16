321637a13c1272b0c0e87e91bb792a565bcde1be
Him188 <Him188@mamoe.net>
2020-09-16 22:15:52 +0800

    Update JvmBlockingBridge documents

--- runtime/src/commonMain/kotlin/net/mamoe/kjbb/JvmBlockingBridge.kt
+++ runtime/src/commonMain/kotlin/net/mamoe/kjbb/JvmBlockingBridge.kt
@@ -3,17 +3,22 @@
 
 package net.mamoe.kjbb
 
+import kotlin.jvm.JvmOverloads
+import kotlin.jvm.JvmStatic
+
 /**
  * Instructs the compiler to generate a blocking bridge for calling suspend function from Java.
+ *
+ * [JvmOverloads] and [JvmStatic] are supported.
  *
  * Example:
  * ```
  * @JvmBlockingBridge
  * suspend fun foo( params ) { /* ... */ }
  *
- * // The compiler generates (visible only for Java):
- * @JvmBlockingBridge
- * fun foo( params ) = runBlocking { foo(params) }
+ * // The compiler generates (visible only from Java):
+ * @GeneratedBlockingBridge
+ * fun foo( params ) = `$runSuspend$` { foo(params) }
  * ```
  */
 @OptionalExpectation
--- runtime/src/jvmMain/kotlin/net/mamoe/kjbb/JvmBlockingBridge.kt
+++ runtime/src/jvmMain/kotlin/net/mamoe/kjbb/JvmBlockingBridge.kt
@@ -5,14 +5,16 @@ package net.mamoe.kjbb
 /**
  * Instructs the compiler to generate a blocking bridge for calling suspend function from Java.
  *
+ * [JvmOverloads] and [JvmStatic] are supported.
+ *
  * Example:
  * ```
  * @JvmBlockingBridge
  * suspend fun foo( params ) { /* ... */ }
  *
- * // The compiler generates (visible only for Java):
- * @JvmBlockingBridge
- * fun foo( params ) = runBlocking { foo(params) }
+ * // The compiler generates (visible only from Java):
+ * @GeneratedBlockingBridge
+ * fun foo( params ) = `$runSuspend$` { foo(params) }
  * ```
  */
 @Target(AnnotationTarget.FUNCTION)
