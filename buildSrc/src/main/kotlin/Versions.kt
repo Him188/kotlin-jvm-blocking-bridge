d37624b78e64339179a7a8941f34039fa8b4900f
Him188 <Him188@mamoe.net>
2020-09-19 13:54:25 +0800

    Publish IJ plugin for 1.1.0 companion

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val project = "1.1.0"
-    const val idePlugin = "1.2.1"
+    const val idePlugin = "1.3.0"
 
     const val kotlin = "1.4.0"
     const val coroutines = "1.3.9"
