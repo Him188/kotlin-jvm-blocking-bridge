7bb44eb3588f1f702c44fd016de9e11eb36b9b41
Him188 <Him188@mamoe.net>
2020-11-08 23:00:29 +0800

    1.4.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,11 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 object Versions {
-    const val project = "1.4.0"
-    const val idePlugin = "1.4.0"
+    const val project = "1.4.1"
+    const val idePlugin = project
 
     const val kotlin = "1.4.20-RC"
-    const val coroutines = "1.3.9"
+    const val coroutines = "1.4.0"
 
     const val bintray = "1.8.5"
 }
