46f227fd6b16ccc83a75c2568cc58674f7c22aa2
Him188 <Him188@mamoe.net>
2020-12-27 17:49:17 +0800

    1.5.1

--- ide-plugin/run/build.gradle.kts
+++ ide-plugin/run/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    id("net.mamoe.kotlin-jvm-blocking-bridge") version "1.5.0"
+    id("net.mamoe.kotlin-jvm-blocking-bridge") version "1.5.1"
 }
 
 group = "me.him188"
