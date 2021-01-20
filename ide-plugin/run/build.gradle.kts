1e92f24d3219c241b51909b46c50c0fa3a251bf2
Him188 <Him188@mamoe.net>
2021-01-20 23:42:59 +0800

    1.7.3

--- ide-plugin/run/build.gradle.kts
+++ ide-plugin/run/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    id("net.mamoe.kotlin-jvm-blocking-bridge") version "1.7.2"
+    id("net.mamoe.kotlin-jvm-blocking-bridge") version "1.7.3"
 }
 
 blockingBridge {
