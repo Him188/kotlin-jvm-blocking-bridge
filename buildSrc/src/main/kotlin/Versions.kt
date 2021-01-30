769ca90006dd95458a749c5cc499e1533971bfd0
Him188 <Him188@mamoe.net>
2021-01-30 18:24:28 +0800

    1.8.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 @file:Suppress("MemberVisibilityCanBePrivate")
 
 object Versions {
-    const val project = "1.8.0-dev-1"
+    const val project = "1.8.0"
     const val idePlugin = project
 
     const val kotlin = "1.4.20"
