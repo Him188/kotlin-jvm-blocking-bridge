eb0b8022076039f0f9d0bf9edd5dd6a354921cea
Him188 <Him188@mamoe.net>
2021-02-04 10:13:21 +0800

    Release v1.9.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 @file:Suppress("MemberVisibilityCanBePrivate")
 
 object Versions {
-    const val project = "1.9.0"
+    const val project = "1.9.1"
     const val idePlugin = project
 
     const val kotlin = "1.4.30"
