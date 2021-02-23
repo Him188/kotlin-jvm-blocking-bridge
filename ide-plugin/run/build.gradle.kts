5ed0733d19241ced2202e0c0835166e8247d5cc9
Him188 <Him188@mamoe.net>
2021-02-23 09:17:04 +0800

    1.10.0-maven-central-2

--- ide-plugin/run/build.gradle.kts
+++ ide-plugin/run/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    id("net.mamoe.kotlin-jvm-blocking-bridge") version "1.10.0-maven-central-1"
+    id("net.mamoe.kotlin-jvm-blocking-bridge") version "1.10.0-maven-central-2"
 }
 
 blockingBridge {
