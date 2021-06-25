becafa75eb97374d0f39f946dae6bac3e9eeb867
Him188 <Him188@mamoe.net>
2021-06-25 17:50:40 +0800

    Use 1.5.20

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,6 +4,6 @@ object Versions {
     const val project = "1.10.6"
     const val idePlugin = project
 
-    const val kotlin = "1.5.0"
+    const val kotlin = "1.5.20"
     const val coroutines = "1.5.0-RC"
 }
