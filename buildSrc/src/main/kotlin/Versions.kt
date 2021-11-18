1d30638c341f11316a506be2de853ff1e896f375
Him188 <Him188@mamoe.net>
2021-11-18 01:19:38 +0000

    Use Kotlin 1.6.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,6 +4,6 @@ object Versions {
     const val project = "1.10.6-160.1"
     const val idePlugin = project
 
-    const val kotlin = "1.6.0-RC2"
+    const val kotlin = "1.6.0"
     const val coroutines = "1.5.2"
 }
