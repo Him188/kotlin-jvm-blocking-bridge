94442027de5b9cdca09df98e5e986fa3eaf347ec
Him188 <Him188@mamoe.net>
2022-05-17 21:47:46 +0100

    Version 2.1.0-162.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@
 object Versions {
     const val intellij = "2022.1"
 
-    const val project = "2.0.0-162.1"
+    const val project = "2.1.0-162.1"
     const val idePlugin = "221-$project"
 
     const val kotlin = "1.6.20"
