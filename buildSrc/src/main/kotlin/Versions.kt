33038dfeee26817b3c98dacfa8b50314025d836c
Him188 <Him188@mamoe.net>
2022-10-23 11:33:11 +0100

    Update to 1.7.20

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val project = "2.2.0-172.1"
     const val idePlugin = "222-$project"
 
-    const val kotlin = "1.7.20-Beta"
+    const val kotlin = "1.7.20"
     const val coroutines = "1.6.1"
 
     const val publicationGroup = "me.him188"
