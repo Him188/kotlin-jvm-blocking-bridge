2355ff9d48c7d534322d2ba523ae1046dfad8155
Him188 <Him188@mamoe.net>
2023-03-18 15:15:12 +0000

    Version 3.0.0-180.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@
 object Versions {
     const val intellij = "2022.3"
 
-    const val project = "2.2.0-180.1"
+    const val project = "3.0.0-180.1"
     const val idePlugin = "223-$project"
 
     const val kotlin = "1.8.0"
